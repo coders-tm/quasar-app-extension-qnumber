6f320ad5d411365860455107bb9295d87bfd29dc
Dipak Sarkar <hello@dipaksarkar.in>
2023-03-15 22:00:35 +0530

    added docs

---
+++ demo/src/components/QCode.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <!-- eslint-disable vue/html-indent -->
+      <q-markdown
+      >
+        Indented code (4 spaces)
+
+        Inline `code`
+
+            // Some comments (4 spaces on all)
+            line 1 of code
+            line 2 of code
+            line 3 of code
+
+        Block code "fences"
+
+        ```
+        Sample text here...
+        ```
+
+        Syntax highlighting
+
+        ```vue
+        var foo = function (bar) {
+          return bar++;
+        };
+
+        console.log(foo(5));
+        ```
+      </q-markdown>
+      <!-- eslint-enable vue/html-indent -->
+    </div>
+  </template>
+
+  <script>
+  import { defineComponent, ref } from 'vue'
+  import { QMarkdown } from '@quasar/quasar-ui-qmarkdown'
+  import '@quasar/quasar-ui-qmarkdown/dist/index.css'
+
+  export default defineComponent({
+    name: 'QCode',
+    components: {
+      QMarkdown
+    },
+
+    setup () {
+      const model = ref(false),
+        model2 = ref(false),
+        model3 = ref(false)
+
+      return {
+        model,
+        model2,
+        model3
+      }
+    }
+  })
+  </script>
--- demo/src/pages/IndexPage.vue
+++ demo/src/pages/IndexPage.vue
@@ -1,11 +1,14 @@
 <template>
   <q-page padding>
     <q-number @update:model-value="onChange" :options="options"  dense outlined v-model="price"/>
+    <!-- <q-code/> -->
   </q-page>
 </template>
 
 <script>
+// import QCode from 'components/QCode.vue'
 export default {
+  // components: { QCode },
   data() {
     return {
       price: 123456.98,
@@ -16,10 +19,7 @@ export default {
   },
   methods: {
     onChange () {
-      console.log('onChange');
-      setTimeout(() => {
-        this.price = 5005.256
-      }, 3000);
+      //
     }
   }
 }
