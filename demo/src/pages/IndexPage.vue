b1f8a485dc27254184d0e25ddf7f5e201b6d793b
Dipak Sarkar <hello@dipaksarkar.in>
2023-04-03 02:24:49 +0530

    updated docs

--- demo/src/pages/IndexPage.vue
+++ demo/src/pages/IndexPage.vue
@@ -1,27 +1,44 @@
 <template>
   <q-page padding>
-    <q-number @update:model-value="onChange" :options="options"  dense outlined v-model="price"/>
-    <CurrencyFormat class="q-mt-lg" v-model="price2"/>
+      <div class="row q-col-gutter-sm">
+        <div class="col-xs-12 col-sm-4">
+          <div class="text-subtitle2">Currency</div>
+          <q-number :options="options"  dense outlined v-model="price"/>
+          <div class="text-secondary">Value: {{ price }}</div>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+          <div class="text-subtitle2">Percentage</div>
+          <q-number :options="optionsParcent"  dense outlined v-model="parcent"/>
+          <div class="text-secondary">Value: {{ parcent }}</div>
+        </div>
+        <div class="col-xs-12 col-sm-4">
+          <div class="text-subtitle2">Reverse Fill</div>
+          <q-number :options="optionsReverseFill"  dense outlined v-model="reverseFill"/>
+          <div class="text-secondary">Value: {{ reverseFill }}</div>
+        </div>
+      </div>
   </q-page>
 </template>
 
 <script>
-import CurrencyFormat from 'components/CurrencyFormat.vue';
-
 export default {
-  components: { CurrencyFormat },
   data() {
     return {
       price: 123456.98,
-      price2: 123456.98,
       options: {
-        prefix: '$'
+        prefix: '£'
+      },
+      parcent: 8.50,
+      optionsParcent: {
+        suffix: '%',
+        min: 0,
+        max: 100
+      },
+      reverseFill: 12438.56,
+      optionsReverseFill: {
+        prefix: '£',
+        reverseFill: true
       }
-    }
-  },
-  methods: {
-    onChange (value) {
-      this.price2 = value * 1.2
     }
   }
 }
